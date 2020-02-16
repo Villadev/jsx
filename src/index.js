c0ed8e3bab30bf10eb9fccc32e3991fab6332dd3
Oriol Villagrasa Camps <oriol.villagrasa@col.vueling.com>
2020-02-16 18:48:49 +0100

    Secció 2: Building content with JSX

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
+function getButtonText() {
+    return 'Pulsa el botó';
+}
+
 // Create a React component
 const App = () => {
-    return <div>Hola que tal?</div>
+    return (
+        <div>
+            <label className="label" htmlFor="name">Posa el teu nom: </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{getButtonText()}</button>
+        </div>
+    )
 };
 
 // Take the React componentand show it on the screen
